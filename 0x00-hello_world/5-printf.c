846ad3601a4f08153c1b60e1af3b2c324b962c16
Bellannj <annabelbliss123@gmail.com>
2023-03-20 10:05:22 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that pints a line eith printf
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+Return: (0);
+}
