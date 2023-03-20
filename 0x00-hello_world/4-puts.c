3dbe1b5ad5e24616823d9a4e739325a834175fa9
Bellannj <annabelbliss123@gmail.com>
2023-03-20 09:13:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - entry point for writting a program that prints the line : Programming is like building a multilingual puzzle with put functn
  * Return: 0 (successfull)
  */
-int main(void )
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
