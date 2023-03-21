f86920c1420b21dac99f758de5033e87367f55c8
Bellannj <annabelbliss123@gmail.com>
2023-03-20 19:11:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!bin/bash/
-echo ZZ
+#include <stdio.h>
+#include <unistd.h>
+/**0
+ * main - a c program that prints a line to the stardard erro
+ * Return 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
