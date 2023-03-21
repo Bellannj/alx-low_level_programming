5f389cb22003efcd19c3cefec917a3629a04203a
Bellannj <annabelbliss123@gmail.com>
2023-03-20 19:27:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-/**0
- * main - a c program that prints a line to the stardard erro
- * Return 1
- */
+/**
+ * main - print a c program that  prints the standard error
+ *
+ * Return: 1
+*/
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
