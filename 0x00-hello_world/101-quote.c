df91bde9456e5c814fa9e01dc256cad2b683241d
Bellannj <annabelbliss123@gmail.com>
2023-04-14 19:04:31 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - a c program that prints a line to the stardard erro
- * Return 1
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\", stdout);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
