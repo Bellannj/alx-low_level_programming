08e4cc8d312ed2d18621864d0757dbb0a501c717
Bellannj <annabelbliss123@gmail.com>
2023-08-24 18:30:28 -0700

    list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,18 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 /**
  * struct list_s -  the singly linked list
  * @str: string
- * @len : string lenght
- * @nexts: points to the next node
  * desciption: singly linked list node structure
  */
 typedef struct list_s
 {
 	char *str;
-	unsigned int len:
-	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
