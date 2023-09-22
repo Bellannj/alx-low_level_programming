74fc23d974e173a5c2cb1d4c5baaf125edf5ac95
Bellannj <annabelbliss123@gmail.com>
2023-09-22 05:32:53 -0700

    102-magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
- * main - is using pointer arithmetic to access a memory location
- * Return: 0
- */
 int main(void)
 {
 	int n;
@@ -21,7 +17,6 @@ int *p;
    */
 
 	*(p + 5) = 98;
-  /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
